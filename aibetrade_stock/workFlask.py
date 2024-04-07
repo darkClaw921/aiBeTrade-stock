f78bf4904c5f7418195b9c6539839fd16b9386d7
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2024-04-07 12:58:03 +0300

    minor

--- aibetrade_stock/workFlask.py
+++ aibetrade_stock/workFlask.py
@@ -15,25 +15,26 @@ PHONE_NUMBER = 'your_phone_number'
 app = Flask(__name__)
 
 # Создаем Telegram клиент
-client = TelegramClient('session_name', API_ID, API_HASH, system_version="4.16.32-vxCUSTOM", device_model='Flask Galaxy S24 Ultra, running Android 14')
+client = TelegramClient('session_name_flask', API_ID, API_HASH, system_version="4.16.32-vxCUSTOM", device_model='Flask Galaxy S24 Ultra, running Android 14')
 client.start()
 
 # Функция для отправки сообщения в Telegram
-def send_message(chatID, message):
-    client.send_message(chatID, message)
+def send_message(chatID, message, chenalID):
+    client.send_message(chatID, message, reply_to=chenalID)
+    # send_message('id чата или ссылка', 'Cамо сообщение', reply_to = "идентификатор группы в чате")
 
 # Обработчик вебхука
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    channel_id = data.get('channel_id')
+    chenalID = data.get('channel_id')
     message = data.get('message')
     chatID=data.get('chat_id')
     # Проверяем, что пришло корректное сообщение
-    if channel_id and message:
+    if chenalID and message:
         # Если идентификатор канала совпадает с ожидаемым, отправляем сообщение в Telegram
         # if channel_id == CHANNEL_ID:
-        send_message(chatID, message)
+        send_message(chatID, message, chenalID)
         return jsonify({'success': True}), 200
     else:
         return jsonify({'error': 'Missing channel ID or message'}), 400
